6a177d6bd7c604c8d3730bc42d02bce8e02b8f59
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2022-05-16 12:49:53 -0600

    adds error catch

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get("/", (req, res) => {
 
 app.use((req, res, next) => {
     res.status(404).send(`Unable to find the requested resource!`)
-});
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
